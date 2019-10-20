32ad7577b969ea47b7f5a102825655fa0639befe
Erwin Wiguna <erwin.wiguna31@gmail.com>
2019-10-21 01:35:44 +0700

    add model

---
+++ src/main/java/com/assessment/model/Tweets.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.assessment.model;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author erwin.wiguna_gue
+ */
+@Entity
+@Table(name = "tweets")
+public class Tweets {
+    @Id
+    private Integer id;
+    
+    @Column(name = "tweet")
+    private String tweet;
+    
+    @Column(name = "screen_name")
+    private String screenName;
+    
+    @Column(name = "tweet_date_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date tweetDateAt;
+    
+    @Column(name = "created_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+    
+    @Column(name = "updated_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedAt;
+    
+    @Column(name = "created_by")
+    private Integer createdBy;
+    
+    @Column(name = "updated_by")
+    private Integer updatedBy;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTweet() {
+        return tweet;
+    }
+
+    public void setTweet(String tweet) {
+        this.tweet = tweet;
+    }
+
+    public String getScreenName() {
+        return screenName;
+    }
+
+    public void setScreenName(String screenName) {
+        this.screenName = screenName;
+    }
+
+    public Date getTweetDateAt() {
+        return tweetDateAt;
+    }
+
+    public void setTweetDateAt(Date tweetDateAt) {
+        this.tweetDateAt = tweetDateAt;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Integer getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(Integer createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Integer getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(Integer updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+    
+    
+}
